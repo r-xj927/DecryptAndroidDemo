66317e92640d1e243d1c0d831fa626b38a360731
r-xj927 <1872555377@qq.com>
2024-01-04 15:01:17 +0800

    bugfix
    bugfix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -7,7 +7,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "36803ae385348d4ea178ff1631fed05a
 "assets/AssetManifest.bin.json": "5d8f501542ba71875c9467f32c1afd9a",
 "assets/AssetManifest.json": "be8b94d15104bd5858d9cba2888e5472",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "32fce58e2acb9c420eab0fe7b828b761",
+"assets/fonts/MaterialIcons-Regular.otf": "51ae374a42e4c3676b3158d40e704899",
 "assets/key/private.pem": "38d299e8ab68eec03cc657b7d947cf33",
 "assets/key/public.pem": "aaf24f72ea5882f8f098ee89b118bc6c",
 "assets/NOTICES": "26fdbe59d0562e847c43794a27b17d07",
@@ -26,9 +26,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "36803ae385348d4ea178ff1631fed05a
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "971cd77286b47b57d18fc9ea5bc9dcff",
-"/": "971cd77286b47b57d18fc9ea5bc9dcff",
-"main.dart.js": "3a8fcb9f1d9b46f078a820946d24f423",
+"index.html": "d4070b4d1ea7af36cb5f3856c75a86a0",
+"/": "d4070b4d1ea7af36cb5f3856c75a86a0",
+"main.dart.js": "93ee5460805174125a28ed991dacadef",
 "manifest.json": "1f1e552a7978334b99ead5def6b3b8b3",
 "version.json": "aa8f8601cee0f6343016b8151e499a83"};
 // The application shell files that are downloaded before a service worker can
